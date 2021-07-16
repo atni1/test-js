ceb1516c37bdb14cb58e38e28e3ab672904be3df
atni1 <babenkoanton25@gmail.com>
2021-07-16 12:24:20 +0600

    transition to arrays

--- function/calcMyShoes.js
+++ function/calcMyShoes.js
@@ -1,42 +1,164 @@
-const model = 'Air jordan 1 low'
+const basket = [
+    {
+        brand: 'Air jordan',
+        model: '1 low',
+        price: 11990,
+        qty: 1
+    },
+    {
+        brand: 'Air jordan',
+        model: '1 high bread',
+        price: 33000,
+        qty: 1
+    },
+    {
+        brand: 'Air jordan',
+        model: '1 high david',
+        price: 9790,
+        qty: 1
+    },
+    {
+        brand: 'Air jordan',
+        model: '1 high travis',
+        price: 15790,
+        qty: 1
+    },
+    {
+        brand: 'Air jordan',
+        model: '5',
+        price: 17990,
+        qty: 1
+    },
+    {
+        brand: 'Adidas',
+        model: 'Yeezy 700 v3',
+        price: 27000,
+        qty: 1
+    },
+    {
+        brand: 'Air jordan',
+        model: '3',
+        price: 9978,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'Air force 1',
+        price: 9790,
+        qty: 1
+    },
+    {
+        brand: 'Adidas',
+        model: 'Dlilard 1',
+        price: 2870,
+        qty: 1
+    },
+    {
+        brand: 'Adidas',
+        model: 'Dlilard 2',
+        price: 4670,
+        qty: 3
+    },
+    {
+        brand: 'Adidas',
+        model: 'Dlilard 3',
+        price: 6150,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'Kobe 11',
+        price: 7190,
+        qty: 1
+    },
+    {
+        brand: 'Adidas',
+        model: 'EQT',
+        price: 10990,
+        qty: 1
+    },
+    {
+        brand: 'Adidas',
+        model: 'iniki',
+        price: 4790,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'Dunk',
+        price: 4590,
+        qty: 1
+    },
+    {
+        brand: 'Adidas',
+        model: 'NMD r2',
+        price: 4590,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'Solder',
+        price: 7990,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'KD 8',
+        price: 10990,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'LeBron 12',
+        price: 11990,
+        qty: 2
+    },
+    {
+        brand: 'Vans',
+        model: 'SKi-8',
+        price: 6780,
+        qty: 1
+    },
+    {
+        brand: 'TNF',
+        model: 'Back-to berlrle',
+        price: 15790,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'Duckboot force',
+        price: 6990,
+        qty: 1
+    },
+    {
+        brand: 'Timberland',
+        model: 'boot',
+        price: 11790,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'Kyri',
+        price: 10790,
+        qty: 1
+    },
+    {
+        brand: 'Nike',
+        model: 'presto',
+        price: 4790,
+        qty: 1
+    },
+]
 
-function getPrice(model) {
-    switch (model) {
-        case 'Air jordan 1 low': return 11990 
-        case 'Air jordan 1 high bread': return 33000
-        case 'Air jordan 1 high david': return 9790
-        case 'Air jordan 1 high travis': return 15790
-        case 'Air jordan 5': return 17990
-        case 'Yeezy 700 v3': return 27000
-        case 'Air jordan 3': return 9978
-        case 'Air force 1': return 9790
-        case 'Dlilard 1': return 2870
-        case 'Dlilard 2': return 4670
-        case 'Dlilard 3': return 6150
-        case 'Kobe 11': return 7190
-        case 'EQT': return 10990
-        case 'iniki': return 4790
-        case 'Dunk': return 4590
-        case 'NMD r2': return 4590
-        case 'Solder': return 7990
-        case 'KD 8': return 10990
-        case 'LeBron 12': return 11990
-        case 'Vans': return 6780
-        case 'TNF': return 15790
-        case 'Duckboot force': return 6990
-        case 'Timberland': return 11790
-        case 'Kyri': return 10790
-        case 'presto': return 4790
-        default: return null
-    }  
+function getTolalPrice(basket) {
+    let tolalBasketPrice = 0
+    for (const prod of basket) {
+        tolalBasketPrice += prod.price *= prod.qty
+    }
+    return tolalBasketPrice
 }
 
-function getPriceByCount(model, count = 1) {
-    const price = getPrice(model)
+console.log('Total spent on sneaker: ' + getTolalPrice(basket) + '₽')
 
-    if (price == null) return null
 
-    return price * count
-}
-
-console.log(getPriceByCount(model) + getPriceByCount('Air jordan 1 high bread') + getPriceByCount('Air jordan 1 high david') +  getPriceByCount('Air jordan 1 high travis') +  getPriceByCount('Air jordan 5') +  getPriceByCount('Yeezy 700 v3') + getPriceByCount('Air jordan 3') + getPriceByCount('Air force 1') + getPriceByCount('Dlilard 1') + getPriceByCount('Dlilard 2', 3) + getPriceByCount('Dlilard 3') + getPriceByCount('Kobe 11') + getPriceByCount('EQT') + getPriceByCount('iniki') + getPriceByCount('Dunk') + getPriceByCount('NMD r2') + getPriceByCount('Solder') + getPriceByCount('KD 8') + getPriceByCount('LeBron 12', 2) + getPriceByCount('Vans') + getPriceByCount('TNF') + getPriceByCount('Duckboot force') + getPriceByCount('Timberland') + getPriceByCount('Kyri') + getPriceByCount('Kyri'))
