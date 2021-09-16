2cd516c509a61a2e7e6b0a8cf94ff3c4ae4c29c8
atni1 <babenkoanton25@gmail.com>
2021-09-16 23:45:26 +0700

    commit

--- test/script.js
+++ test/script.js
@@ -1,57 +1,75 @@
-alert('Данный сайт предназначен для лиц старше 18 лет. Выполните дальнейшие действия для доступа с сайту');
-var age = +prompt('Введите Ваш возраст');
-function ageTest(age) {
-    if (age < 18){
-        alert('В доступе отказано');
-    }  
-    if (age >= 18) {
-        alert('Добро пожаловать на сайт')
-        var chooseCategories = prompt('Азартные игры, ставки на спорт, рулетка, игровые автоматы')
-        switch (chooseCategories) {
-            case 'азартные игры':
-                var gambling = prompt('Покер, 21')
-                switch (gambling) {
-                    case 'покер':
-                        alert('Правила игры "Покер": \nhttps://academypoker.ru/rules.html')
-                        break;
-                    case '21':
-                        alert('Правила игры "21": \nhttps://www.keft.ru/help/bj/rules')
-                        break;
+// alert('Данный сайт предназначен для лиц старше 18 лет. Выполните дальнейшие действия для доступа с сайту');
+// var age = +prompt('Введите Ваш возраст');
+// function ageTest(age) {
+//     if (age < 18){
+//         alert('В доступе отказано');
+//     }  
+//     if (age >= 18) {
+//         alert('Добро пожаловать на сайт')
+//         var chooseCategories = prompt('Азартные игры, ставки на спорт, рулетка, игровые автоматы')
+//         switch (chooseCategories) {
+//             case 'азартные игры':
+//                 var gambling = prompt('Покер, 21')
+//                 switch (gambling) {
+//                     case 'покер':
+//                         alert('Правила игры "Покер": \nhttps://academypoker.ru/rules.html')
+//                         break;
+//                     case '21':
+//                         alert('Правила игры "21": \nhttps://www.keft.ru/help/bj/rules')
+//                         break;
                 
-                    default:
-                        alert('Используйте указанные игры')
-                        break;
-                }
-                break;
-            case 'ставки на спорт':
-                var bet = prompt('Футбол, баскетбол')
-                switch (bet) {
-                    case 'футбол':
-                        alert('Правила игры "Футбол": \nhttps://academypoker.ru/rules.html')
-                        break;
-                    case 'баскетбол':
-                        alert('Правила игры "Баскетбол": \nhttps://academypoker.ru/rules.html')
-                        break;
+//                     default:
+//                         alert('Используйте указанные игры')
+//                         break;
+//                 }
+//                 break;
+//             case 'ставки на спорт':
+//                 var bet = prompt('Футбол, баскетбол')
+//                 switch (bet) {
+//                     case 'футбол':
+//                         alert('Правила игры "Футбол": \nhttps://academypoker.ru/rules.html')
+//                         break;
+//                     case 'баскетбол':
+//                         alert('Правила игры "Баскетбол": \nhttps://academypoker.ru/rules.html')
+//                         break;
                 
-                    default:
-                        alert('Используйте указанные игры')
-                        break;
-                }
-                break;
-            case 'рулетка':
-                alert('Добро пожаловать в игру рулетка!')
-                break;
-            case 'игровые автоматы':
-                alert('Добро пожаловать в раздел игровые автоматы!')
-                break;
+//                     default:
+//                         alert('Используйте указанные игры')
+//                         break;
+//                 }
+//                 break;
+//             case 'рулетка':
+//                 alert('Добро пожаловать в игру рулетка!')
+//                 break;
+//             case 'игровые автоматы':
+//                 alert('Добро пожаловать в раздел игровые автоматы!')
+//                 break;
         
-            default:
-                alert('Выберите один из указанных разделов!')
-                // Возвращаться к выбору категории
-                break;
-        }
-    } 
+//             default:
+//                 alert('Выберите один из указанных разделов!')
+//                 // Возвращаться к выбору категории
+//                 break;
+//         }
+//     } 
+// }
+// ageTest(age)
+var table = "<table border='1' width='600'>";
+var i = 1;
+while (i <= 100) {
+    // if (i == 10) {
+    //     break;
+    // }
+    var color = i % 2 == 0 ? "red" : "blue";
+    table += "<tr style='color:" + color + "'>";
+    var j = 1;
+    while (j <= 10) {
+        table += "<td>" + j * i + "</td>";
+        j++;
+    }
+    table += "</tr>";
+    i++;
 }
-ageTest(age)
+table += "</table>"
+document.write(table)
 
 
